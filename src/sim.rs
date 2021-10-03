2dea2f668416b5ec272a80963e9502cb38becc3c
Brian Anderson <andersrb@gmail.com>
2021-10-02 19:52:00 -0600

    .

--- src/sim.rs
+++ src/sim.rs
@@ -12,16 +12,22 @@ pub fn simulate_call(view: &AuctionPlayerView) -> SimulatedCalls {
     let opening = view.opening();
 
     if opening {
-        let hcps = view.high_card_points();
-        let have_12plus_hcps = 12 <= hcps;
-        let have_15_to_17_hcps = 15 <= hcps && hcps <= 117;
-        let balanced = view.balanced();
+        play_opening(view)
+    } else {
+        todo!()
+    }
+}
 
-        if have_15_to_17_hcps && balanced {
-            bid(1, BidSuit::NoTrump)
-        } else {
-            todo!()
-        }
+fn play_opening(view: &AuctionPlayerView) -> SimulatedCalls {
+    assert!(view.opening());
+
+    let hcps = view.high_card_points();
+    let have_12plus_hcps = 12 <= hcps;
+    let have_15_to_17_hcps = 15 <= hcps && hcps <= 117;
+    let balanced = view.balanced();
+
+    if have_15_to_17_hcps && balanced {
+        bid(1, BidSuit::NoTrump)
     } else {
         todo!()
     }
