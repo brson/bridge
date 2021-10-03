f099873030c2653694b447efdc59b798b65a7d75
Brian Anderson <andersrb@gmail.com>
2021-10-03 13:51:36 -0600

    add console_error_panic_hook

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,8 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn set_panic_hook() {
+    console_error_panic_hook::set_once();
+}
