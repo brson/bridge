99afae7be4a6877f5e618d7756d606c2930daac7
Brian Anderson <andersrb@gmail.com>
2021-10-03 16:00:49 -0600

    .

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,10 +1,6 @@
 use log::info;
 use wasm_bindgen::prelude::*;
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn init() {
     console_error_panic_hook::set_once();
