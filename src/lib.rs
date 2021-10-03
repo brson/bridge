321ceb2cf318522f756b8b9818b47636ba3c54b7
Brian Anderson <andersrb@gmail.com>
2021-10-03 13:55:11 -0600

    .

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,5 @@ pub mod defs;
 pub mod sim;
 pub mod eval;
 
-use wasm_bindgen::prelude::*;
-
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-pub fn set_panic_hook() {
-    console_error_panic_hook::set_once();
-}
+#[cfg(target = "wasm32-unknown-unknown")]
+pub mod wasm;
